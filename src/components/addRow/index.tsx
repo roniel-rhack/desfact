6427361de55d12b90e6c364e186d8d6946f8aa83
Roniel <roniel.rack@gmail.com>
2020-04-11 23:46:12 -0400

    Warnings deleted

--- src/components/addRow/index.tsx
+++ src/components/addRow/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import Page from './page';
 import {connect} from "react-redux";
 import saveRecord from "../../redux/actions/saveRecord";
--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import AddRow from "../../components/addRow";
 
 
