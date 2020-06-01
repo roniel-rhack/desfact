eb57e863dd02238611246028c546d92cc1d7973c
Roniel <roniel.rack@gmail.com>
2020-05-31 20:02:54 -0400

    schemaValidation.ts updated

--- src/components/DesgloseField.tsx
+++ src/components/DesgloseField.tsx
@@ -5,41 +5,56 @@ import {Button} from "@material-ui/core";
 import InputAdornment from "@material-ui/core/InputAdornment";
 
 interface iProps extends FieldAttributes<any> {
-    index: number;
-    submitCount: number;
-    arrayHelpers: FieldArrayRenderProps;
-    error?: any;
+  index: number;
+  submitCount: number;
+  arrayHelpers: FieldArrayRenderProps;
+  error?: any;
 }
 
 const DesgloseField = (props: iProps) => {
-    return (
-        <div style={{
-            borderBottom: "solid 1px silver",
-            padding: 5,
-            borderRadius: 15
-        }}>
-            <Field name={`desgloses.${props.index}.cuenta`} as={TextField}
-                   error={props.error?.cuenta && props.submitCount > 0}
-                   placeholder="Cuenta" size="small"
-                   helperText={props.submitCount > 0 ? props.error?.cuenta : undefined}
-                   style={{marginRight: 20, width: 200}}/>
-            <Field type="number" step={0.1} name={`desgloses.${props.index}.imp_cargo`}
-                   error={props.error?.imp_cargo && props.submitCount > 0}
-                   as={TextField} size="small" helperText={props.submitCount > 0 ? props.error?.imp_cargo : undefined}
-                   placeholder="Importe" style={{marginRight: 40, width: 200}} InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        $
-                    </InputAdornment>
-                ),
-            }}/>
-            <Button variant="outlined" size="small"
-                    onClick={() => props.arrayHelpers.remove(props.index)}
-                    style={{
-                        borderRadius: 50
-                    }}>Eliminar</Button>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        borderBottom: "solid 1px silver",
+        padding: 5,
+        borderRadius: 15,
+      }}
+    >
+      <Field
+        name={`desgloses.${props.index}.cuenta`}
+        as={TextField}
+        error={props.error?.cuenta && props.submitCount > 0}
+        placeholder="Cuenta"
+        size="small"
+        helperText={props.submitCount > 0 ? props.error?.cuenta : undefined}
+        style={{marginRight: 20, width: 200}}
+      />
+      <Field
+        type="number"
+        step={0.1}
+        name={`desgloses.${props.index}.imp_cargo`}
+        error={props.error?.imp_cargo && props.submitCount > 0}
+        as={TextField}
+        size="small"
+        helperText={props.submitCount > 0 ? props.error?.imp_cargo : undefined}
+        placeholder="Importe"
+        style={{marginRight: 40, width: 200}}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">$</InputAdornment>,
+        }}
+      />
+      <Button
+        variant="outlined"
+        size="small"
+        onClick={() => props.arrayHelpers.remove(props.index)}
+        style={{
+          borderRadius: 50,
+        }}
+      >
+        Eliminar
+      </Button>
+    </div>
+  );
+};
 
 export default DesgloseField;
--- src/utils/schemaValidation.ts
+++ src/utils/schemaValidation.ts
@@ -2,27 +2,57 @@ import * as yup from "yup";
 import {desgloseInterface} from "./desgloseDefault";
 
 const validationSchema = yup.object({
-    fileName: yup.string()
-        .required('Es obligatorio que asigne un nombre de archivo')
-        .max(100, 'El nombre del archivo no puede tener más de 100 caracteres.'),
-    desgloses: yup.array().of(yup.object<desgloseInterface>({
-        cuenta: yup.string()
-            .length(14, 'La cuenta debe tener una longitud de 14 digitos.')
-            .required('La cuenta es obligatoria.')
-            .test('only-numbers', "Este campo solo puede contener números.", (value) => {
-                // eslint-disable-next-line
-                return value == parseInt(value);
-            }),
-        imp_cargo: yup.number()
-            .required("El importe es obligatorio.")
-            .min(1, "El importe a desglosar debe ser mayor que $ 1.00.")
-            .test('rounds', "El importe no puede tener más de dos lugares decimales.", value => {
-                // eslint-disable-next-line eqeqeq
-                return value == value?.toFixed(2);
-            }),
-        contab_ban: yup.boolean().required(),
-        pendiente: yup.number().required()
-    }))
-})
+  fileName: yup
+    .string()
+    .required("Es obligatorio que asigne un nombre de archivo")
+    .max(100, "El nombre del archivo no puede tener más de 100 caracteres."),
+  desgloses: yup.array().of(
+    yup.object<desgloseInterface>({
+      cuenta: yup
+        .string()
+        .length(14, "La cuenta debe tener una longitud de 14 digitos.")
+        .required("La cuenta es obligatoria.")
+        .test(
+          "only-numbers",
+          "Este campo solo puede contener números.",
+          (value) => {
+            // eslint-disable-next-line
+            return value == parseInt(value);
+          }
+        )
+        .test(
+          "cod_moneda",
+          "El código de moneda debe ser CUP (40).",
+          (value) => {
+            return value.slice(0, 2) === "40";
+          }
+        )
+        .test(
+          "cod_contra",
+          "El cod_contra de la cuenta debe ser alguno de los siguientes: (1344, 1345, 1348).",
+          (value) => {
+            return ["1344", "1345", "1348"].includes(value.slice(2, 6));
+          }
+        )
+        .test("tip_contra", "El tip_contra debe ser 8.", (value) => {
+          return value.slice(6, 7) === "8";
+        }),
+      imp_cargo: yup
+        .number()
+        .required("El importe es obligatorio.")
+        .min(1, "El importe a desglosar debe ser mayor que $ 1.00.")
+        .test(
+          "rounds",
+          "El importe no puede tener más de dos lugares decimales.",
+          (value) => {
+            // eslint-disable-next-line eqeqeq
+            return value == value?.toFixed(2);
+          }
+        ),
+      contab_ban: yup.boolean().required(),
+      pendiente: yup.number().required(),
+    })
+  ),
+});
 
 export default validationSchema;
