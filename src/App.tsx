41333a932d747d74f8b62f86ab2d9e5ed09b8719
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-06-08 23:32:00 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import Home from './pages/home'
 import {HashRouter, Route, Switch} from "react-router-dom";
-
-
 import theme from "./theme";
 import {ThemeProvider} from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-const App: React.FC = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <HashRouter>
-                <Switch>
-                    <Route path='/' component={Home}/>
-                </Switch>
-            </HashRouter>
-        </ThemeProvider>
-    );
-};
+const App: React.FC = () => (
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <HashRouter>
+            <Switch>
+                <Route path='/' component={Home}/>
+            </Switch>
+        </HashRouter>
+    </ThemeProvider>
+);
 
 export default App;
